036a503870b32aa57927b39237230b0e544a67d9
Ziyue Yang <yzylivezh@hotmail.com>
2018-07-27 15:51:23 +0800

    update rico benchmark seed

--- benchmark/rico/seed.sh
+++ benchmark/rico/seed.sh
@@ -1,5 +1,5 @@
 root_path=/home/yzy/humanoid/
-humanoid_server=162.105.87.118:43997
+humanoid_server=162.105.87.118:60109
 
 out_tester=_humanoid
 # out_tester=_monkey
@@ -15,18 +15,22 @@ sleep 60
 adb connect localhost:$2
 
 # DROIDBOT
-timeout 20000s droidbot -d localhost:$2 -a $root_path/apps/$1.apk -interval 3 -count 2000 -policy dfs_greedy -grant_perm -keep_env -keep_app -random -is_emulator -humanoid $humanoid_server -o $root_path/out$out_tester/$1/droidbot_out &> $root_path/out$out_tester/$1/droidbot.log &
+# timeout 20000s droidbot -d localhost:$2 -a $root_path/apps/$1.apk -interval 3 -count 2000 -policy dfs_greedy -grant_perm -keep_env -keep_app -random -is_emulator -humanoid $humanoid_server -o $root_path/out$out_tester/$1/droidbot_out &> $root_path/out$out_tester/$1/droidbot.log &
+
+# MONKEY
+package_name=$(aapt dump badging $root_path/apps/$1.apk |grep 'package\: name' | awk -F' ' '{print $2}' | awk -F"'" '{print $2}')
+timeout 600s adb -s localhost:$2 install $root_path/apps/$1.apk
+timeout 20000s adb -s localhost:$2 monkey -p $package_name --ignore-crashes --ignore-security-exceptions --throttle 500 -v 10000
 
 tester_pid=$!
 ec_count=1
 
 while kill -0 "$tester_pid" > /dev/null 2>&1; do
-    sleep 600
-    adb -s localhost:$2 shell am broadcast -a edu.gatech.m3.emma.COLLECT_COVERAGE
-    adb -s localhost:$2 pull /sdcard/coverage.ec $root_path/out$out_tester/$1/coverage.ec.$ec_count
+    sleep 1
+    echo "$ec_count" >> $root_path/out$out_tester/$1/activity_coverage
+    adb -s localhost:$2 shell dumpsys activity activities | grep 'Hist #' >> $root_path/out$out_tester/$1/activity_coverage
     let ec_count++
 done
 
 adb -s localhost:$2 pull /sdcard/coverage.ec $root_path/out$out_tester/$1/coverage.ec
 adb -s localhost:$2 shell reboot -p
-
